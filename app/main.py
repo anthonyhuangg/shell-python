fa9f10a7d3aeef68ce47a9aeeb064e1df3a9ec97
anthonyhuangg <anthony.huang4@gmail.com>
2025-11-15 23:57:18 +1100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ def check_valid_command(command):
     return False
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    
-    if not check_valid_command(command):
-        print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        
+        if not check_valid_command(command):
+            print(f"{command}: command not found")
 
-    pass
 
 if __name__ == "__main__":
     main()
