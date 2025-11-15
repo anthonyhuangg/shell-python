40fa9a093c70cd62c62ed137f00ae82a522974ba
anthonyhuangg <anthony.huang4@gmail.com>
2025-11-16 01:53:07 +1100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
 import sys
 
-def check_valid_command(command):
-    return False
+BUILTINS = {"exit"}
+
+def run_builtin(args):
+    cmd = args[0]
+    if cmd == "exit":
+        if len(args) > 1:
+            code = int(args[1])
+        else:
+            code = 0
+        sys.exit(code)
 
 def main():
     while True:
-        sys.stdout.write("$ ")
-        command = input()
-        
-        if not check_valid_command(command):
-            print(f"{command}: command not found")
+        try:
+            sys.stdout.write("$ ")
+            line = input()
+            args = line.split()
+
+            if args[0] not in BUILTINS:
+                print(f"{args[0]}: command not found")
+
+            run_builtin(args)
+        except KeyboardInterrupt:  # Ctrl-C
+            print()
+            continue
 
 
 if __name__ == "__main__":
